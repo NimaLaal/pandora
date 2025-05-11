c246bca135352dc7f854407cff95a8c8f7d70ea1
NimaLaal <81637455+NimaLaal@users.noreply.github.com>
2025-05-11 18:53:14 -0500

    Update nf_dist.py
    generalize the NF wrapper for all types of NF objects

--- pandora/nf_dist.py
+++ pandora/nf_dist.py
@@ -4,6 +4,7 @@ import torch
 import torch.utils.dlpack as td
 import jax.numpy as jnp
 import torch.utils.dlpack as torchdlpack
+torch.set_default_dtype(torch.float64)
 
 
 class NFastroinference(object):
@@ -14,7 +15,7 @@ class NFastroinference(object):
     """
 
     def __init__(
-        self, pyro_nf_object, mean, half_range, scale, gwb_freq_idxs, ast_param_idxs
+        self, pyro_nf_object, nf_type, mean, half_range, scale, gwb_freq_idxs, ast_param_idxs
     ):
         self.nf = pyro_nf_object
         self.B = scale
@@ -22,13 +23,16 @@ class NFastroinference(object):
         self.mean_ast = mean[ast_param_idxs]
         self.half_range_gwb = half_range[gwb_freq_idxs]
         self.half_range_ast = half_range[ast_param_idxs]
-
+        self.nf_type = nf_type
+        if self.nf_type not in ['marg', 'reversed', 'standard']:
+            raise ValueError('`nf_type` must be one of marg, reversed, or standard.')
+        
     def convert_torch_to_numpy(self, torch_tensor):
         # return np.from_dlpack(torch_tensor)
         return torch_tensor.detach().numpy()
 
     def convert_numpy_to_torch(self, numpy_array):
-        return torchdlpack.from_dlpack(numpy_array.__dlpack__()).float()
+        return torchdlpack.from_dlpack(numpy_array.__dlpack__())
 
     def transform_rho_to_scaled_interval(self, gwb_rho):
         return self.B * (gwb_rho - self.mean_gwb) / self.half_range_gwb
@@ -43,31 +47,49 @@ class NFastroinference(object):
         return ast_params * self.half_range_ast / self.B + self.mean_ast
 
     def log_prob(self, gwb_rho, astro_params):
-        scaled_ast = self.convert_numpy_to_torch(
-            self.transform_params_to_scaled_interval(astro_params)
-        )
         scaled_gwb = self.convert_numpy_to_torch(
             self.transform_rho_to_scaled_interval(gwb_rho)
         )
-        return self.convert_torch_to_numpy(
-            self.nf.condition(scaled_ast).log_prob(scaled_gwb)
+        if self.nf_type == 'standard':
+            scaled_ast = self.convert_numpy_to_torch(
+                self.transform_params_to_scaled_interval(astro_params)
+            )
+            return self.convert_torch_to_numpy(
+                self.nf.condition(scaled_ast).log_prob(scaled_gwb)
         )
+        elif self.nf_type == 'reversed':
+            scaled_ast = self.convert_numpy_to_torch(
+                self.transform_params_to_scaled_interval(astro_params)
+            )
+            return self.convert_torch_to_numpy(
+                self.nf.condition(scaled_gwb).log_prob(scaled_ast)
+        )
+        elif self.nf_type == 'marg':
+            return self.convert_torch_to_numpy(
+                self.nf.log_prob(scaled_gwb)
+        )           
 
-    def sample(self, batch_shape, astro_params):
-        scaled_ast = self.convert_numpy_to_torch(
-            self.transform_params_to_scaled_interval(astro_params)
-        )
-        scaled_gwb = self.convert_torch_to_numpy(
-            self.nf.condition(scaled_ast).sample(batch_shape)
-        )
-        return self.transform_rho_to_physical_interval(scaled_gwb)
-
-    def avg_sample_and_prob_of_avg(self, batch_shape, astro_params):
-        scaled_ast = self.convert_numpy_to_torch(
-            self.transform_params_to_scaled_interval(astro_params)
-        )
-        scaled_gwb_avg = torch.mean(
-            self.nf.condition(scaled_ast).sample(batch_shape), dim=0
-        )[None]
-        log_prob = self.nf.condition(scaled_ast).log_prob(scaled_gwb_avg)
-        return self.transform_rho_to_physical_interval(self.convert_torch_to_numpy(scaled_gwb_avg))[0], self.convert_torch_to_numpy(log_prob)[0]
+    def sample(self, batch_shape, context_params):
+        if self.nf_type == 'standard':
+            scaled_ast = self.convert_numpy_to_torch(
+                self.transform_params_to_scaled_interval(context_params)
+            )
+            scaled_gwb = self.convert_torch_to_numpy(
+                self.nf.condition(scaled_ast).sample(batch_shape)
+            )
+            return self.transform_rho_to_physical_interval(scaled_gwb)
+        
+        elif self.nf_type == 'reversed':
+            scaled_gwb = self.convert_numpy_to_torch(
+                self.transform_rho_to_scaled_interval(context_params)
+            )
+            scaled_ast = self.convert_torch_to_numpy(
+                self.nf.condition(scaled_gwb).sample(batch_shape)
+            )
+            return self.transform_params_to_physical_interval(scaled_ast)
+        
+        elif self.nf_type == 'marg':
+            scaled_gwb = self.convert_torch_to_numpy(
+                self.nf.sample(batch_shape)
+            )
+            return self.transform_rho_to_physical_interval(scaled_gwb)
