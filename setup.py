31639b82280f03f344a793b2db7dbc047e0f8c7a
NimaLaal <81637455+NimaLaal@users.noreply.github.com>
2025-07-15 10:39:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ import numpy as np
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
 setup(
     name='pandora',
     version='1.00',
@@ -16,20 +18,7 @@ setup(
     author_email='nima.laal@gmail.com',
     packages=['pandora'],
     zip_safe=False,
-    install_requires=[
-                    'cloudpickle>=3.1.1',
-                    'corner>=2.2.2',
-                    'Cython>=0.29.36',
-                    'h5py>=3.9.0',
-                    'jax>=0.4.19',
-                    'numpy>=2.2.3',
-                    'ptmcmcsampler>=2.1.1',
-                    'pyro_ppl>=1.8.6',
-                    'scipy>=1.15.1',
-                    'setuptools>=68.2.2',
-                    'torch>=2.1.0',
-                    'tqdm>=4.66.1'
-    ],
+    install_requires=requirements,
     include_dirs=[np.get_include()],
     long_description=long_description,
     long_description_content_type='text/markdown',
